7d230a437ef515489940acc6e45401c0186c9b1f
David810 <david81091@gmail.com>
2015-10-01 18:05:28 +0200

    e1

---
+++ 01/e64.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$numero=mt_rand(100,200);
+$almacena=$numero;
+?>
+<b><u>N&uacute;mero aleatorio entre 100 y 200, ambos inclusive</u></b>
+<br>
+<h1>
+<?php
+	echo $almacena;
+?>
+</h1>
+</body>
+</html>
