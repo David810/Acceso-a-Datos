ed4e124d2900e7e7db1b3959063ebc388f0d9a5b
David810 <david81091@gmail.com>
2015-10-29 19:10:56 +0100

    02

---
+++ 02/e80.php
@@ -0,0 +1,53 @@
+<?php 
+$a = rand(100, 200);
+$b = rand(100, 200);
+$c = rand(100, 200);
+$d = rand(100, 200);
+$e = rand(100, 200);
+$f = rand(100, 200);
+ ?>
+
+<table> 
+	<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+	
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		España
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Portugal
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Francia
+	</td>
+	</tr>
+	
+	<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Poblacion
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$a");?>
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$b");?>
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$c");?>
+	</td>
+	</tr>
+	<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Extension
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$d");?>
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$e");?>
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$f");?>
+	</td>
+	</tr>
