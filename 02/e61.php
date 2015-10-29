5acbda2792d293ac20570c76313ecf75af0f6a83
David810 <david81091@gmail.com>
2015-10-29 19:36:55 +0100

    02

---
+++ 02/e61.php
@@ -0,0 +1,60 @@
+<?php 
+$a = rand(1, 100);
+$b = rand(1, 100);
+$c = rand(1, 100);
+$d = rand(1, 100);
+$e = rand(1, 100);
+$f = rand(1, 100);
+ ?>
+
+<table> 
+	<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Pais
+	</td>
+	
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Numero
+	</td>
+	</tr>
+	
+	<tr>
+	
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		España
+	</td>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$a");?>
+	</td>
+	</tr>
+	<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Francia
+	</td>
+
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$b");?>
+	</td>
+
+	</tr>
+		<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Portugal
+	</td>
+
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$c");?>
+	</td>
+
+	</tr>
+		<tr>
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		Japon
+	</td>
+
+	<td style="border:1px solid black; padding:10px; border-spacing:0px">
+		<?php  printf("$d");?>
+	</td>
+
+	</tr>
+</table>
